337809ca87d2d99499709665c63aac2b2ed93f5d
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2022-06-08 10:58:11 +0530

    Adding main.js a JavaScript file

---
+++ 5th Day/main.js
@@ -0,0 +1,27 @@
+var name = 'JAYESH';
+var a = [1,2,3, 'JAYESH', true, false]
+
+var student = {
+    name: 'Jayesh',
+    age: 22,
+    graduation: 'BSc',
+    covidVaccine: true,
+};
+
+function Roll(){
+ console.log('Roll No.')   
+}
+function Div(){
+    setTimeout(() => {
+        console.log('Div.')   
+    }, 2000);
+ 
+}
+
+function Programm(){
+ console.log('Programm')   
+}
+
+Roll();
+Div();
+Programm(); 
