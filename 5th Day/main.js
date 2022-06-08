b73164177abfc41b438c77df4be21f1f4f30de59
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2022-06-08 13:52:49 +0530

    Update main.js

--- 5th Day/main.js
+++ 5th Day/main.js
@@ -22,16 +22,6 @@ function Programm(){
  console.log('Programm')   
 }
 
-function arithmetic(a, b) { console.log(a + b); 
-                            console.log(b - a); 
-                            console.log(a * b); 
-                            console.log(b / a); 
-                            console.log(a % b);
-                            console.log( a++);
-                            console.log(b--);
-                            console.log( a ** 2);
-}
-
 Roll();
 Div();
 Programm(); 
