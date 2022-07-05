7b04ad6bb4591c5d4db7690a328801422b34a4f9
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2022-07-05 12:47:07 +0530

    modification

---
+++ 8th-Day/Async-Await.js
@@ -0,0 +1,6 @@
+async function getData() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/albums');
+    const data = await response.json();
+    console.log(data);
+};
+getData();
